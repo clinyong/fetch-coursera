d08fc90cf4a3982f58f2062f2572ae4c8b9905a5
clinyong <clinyong@gmail.com>
2018-03-25 17:20:25 +0800

    fix create note

--- src/evernote.ts
+++ src/evernote.ts
@@ -1,4 +1,6 @@
 import * as puppeteer from "puppeteer";
+import * as fse from "fs-extra";
+import * as path from "path";
 import * as creds from "../config/creds";
 import { wait } from "./utils";
 
@@ -15,11 +17,14 @@ const nodes = {
 	NOTEBOOK_NAME_INPUT:
 		"#gwt-debug-CreateNotebookDialog-centeredTextBox-textBox",
 	NOTEBOOK_CREATE_ACTION: "#gwt-debug-CreateNotebookDialog-confirm",
-    NOTEBOOK_ITEM: "#gwt-debug-notebooksDrawerSlidingPanel .qa-notebookWidget",
-    CONFIRM_DELETE_NOTEBOOK: "#gwt-debug-ConfirmationDialog-confirm"
+	NOTEBOOK_ITEM: "#gwt-debug-notebooksDrawerSlidingPanel .qa-notebookWidget",
+	CONFIRM_DELETE_NOTEBOOK: "#gwt-debug-ConfirmationDialog-confirm",
+	SHOW_CREATE_NOTE: "#gwt-debug-Sidebar-newNoteButton-container",
+	NOTE_CONTENT: "#gwt-debug-NoteContentEditorView-root",
+	CREATE_NOTE: "#gwt-debug-NoteAttributes-doneButton"
 };
 
-(async function() {
+async function createNote(courseName: string) {
 	const browser = await puppeteer.launch({
 		headless: false
 	});
@@ -48,7 +53,7 @@ const nodes = {
 
 	const notebookList = await page.$$(nodes.NOTEBOOK_ITEM);
 	let found = false;
-	const notebookName = "tttt-alg";
+	const notebookName = courseName;
 	for (let i = 0; i < notebookList.length; i++) {
 		if (found) break;
 
@@ -62,22 +67,59 @@ const nodes = {
 			if (name === notebookName) {
 				notebookItem.hover();
 				await page.waitForFunction(wait);
-                const deleteItem = await notebookItem.$(".qa-deleteButton");
-                await deleteItem.click();
-                await page.waitForFunction(wait);
-                await page.click(nodes.CONFIRM_DELETE_NOTEBOOK);
+				const deleteItem = await notebookItem.$(".qa-deleteButton");
+				await deleteItem.click();
+				await page.waitForFunction(wait);
+				await page.click(nodes.CONFIRM_DELETE_NOTEBOOK);
 			}
 		}
 	}
 
-    await page.waitForFunction(wait);
-	await page.click(EVERNOTE_ADD_NOTEBOOK_BUTTON);
-	await page.waitForSelector(nodes.NOTEBOOK_NAME_INPUT, {
-		visible: true
-	});
+	await page.waitForFunction(wait);
+	const showAddBTN = await page.$(EVERNOTE_ADD_NOTEBOOK_BUTTON);
 
+	await page.waitForFunction(wait);
+	await page.waitForFunction(wait);
+	await page.waitForFunction(wait);
+	await page.waitForFunction(wait);
+	await page.waitForFunction(wait);
+	await page.waitForFunction(wait);
+
+	await page.click(EVERNOTE_ADD_NOTEBOOK_BUTTON);
+	await page.waitForFunction(wait);
 	await page.click(nodes.NOTEBOOK_NAME_INPUT);
+	await page.waitForFunction(wait);
 
 	await page.keyboard.type(notebookName);
 	await page.click(nodes.NOTEBOOK_CREATE_ACTION);
-})();
+
+	const subtitlePath = path.resolve(
+		__dirname,
+		`../data/${courseName}/subtitle`
+	);
+	const subtitleFiles = await fse.readdir(subtitlePath);
+	for (let i = 0; i < 1; i++) {
+		const filename = subtitleFiles[i];
+		const contentLines = (await fse.readFile(
+			path.join(subtitlePath, filename),
+			"utf8"
+		)).split("\n");
+		await page.waitForFunction(wait);
+		await page.click(nodes.SHOW_CREATE_NOTE);
+		await page.waitForFunction(wait);
+		await page.keyboard.type(filename);
+		await page.waitForFunction(wait);
+		await page.click(nodes.NOTE_CONTENT);
+		await page.waitForFunction(wait);
+
+		for (let j = 0; j < contentLines.length; j++) {
+			await page.keyboard.type(contentLines[j]);
+			await page.keyboard.press("Enter");
+		}
+
+		await page.waitForFunction(wait);
+		await page.click(nodes.CREATE_NOTE);
+	}
+}
+
+createNote("algorithms-part1");
